5294fde5ba5ccb1024cb2569046044a006ad24c2
BYRON LOARTE <by_tosh20@hotmail.com>
2025-08-26 16:57:58 +0000

    fundamentos

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link, Outlet, useLocation } from 'react-router'
+
+
+const LandingPage = () => 
+{
+
+  const location = useLocation()
+  const urlActual = location.pathname
+
+  return (
+    <div className='md:flex md:min-h-screen'>
+
+      <div className='md:w-1/4 bg-sky-900 px-5 py-10'>
+
+        <h2 className='text-4xl font-black text-center text-white underline'>Fundamentos</h2>
+
+        <nav className='mt-10'>
+
+          <Link to='/fundamentos' className={`${urlActual === '/fundamentos' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>React</Link>
+
+          <Link to='/usestate' className={`${urlActual === '/usestate' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>useState</Link>
+          
+          <Link to='/useffect' className={`${urlActual === '/useffect' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>useEffect</Link>
+          
+          <Link to='/customhook' className={`${urlActual === '/customhook' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>customHook</Link>
+          
+          <Link to='/props' className={`${urlActual === '/props' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>Props</Link>
+          
+          <Link to='/zustand' className={`${urlActual === '/zustand' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>Zustand</Link>
+          
+          <Link to='/rederizado' className={`${urlActual === '/rederizado' ? 'text-blue-300 underline' : 'text-white'} text-2xl block mt-2 hover:text-blue-200`}>Renderizado</Link>
+          
+        </nav>
+
+      </div>
+
+      <div className='md:w-3/4 p-10 md:h-screen overflow-y-scroll'>
+
+          <Outlet/>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default LandingPage
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',
+    allowedHosts: ['98e93ea5-9538-4ff9-9373-c75809af2597-00-3tonq8itozctu.picard.replit.dev']
   }
 })
